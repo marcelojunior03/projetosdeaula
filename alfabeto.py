48279596e81c1819b1d5c395683a2e3705b439f5
marcelojunior03 <marceloheiffigjr@hotmail.com>
2025-05-26 22:23:30 -0300

    Add files via upload

---
+++ alfabeto.py
@@ -0,0 +1,10 @@
+lista = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+indice = lista.index
+
+separar = []
+palavra = input('Digite uma palavra: ')
+separar = list(palavra)
+
+formatada = 
+
+print(f'Palavra escrita: {palavra}\nPalavra formatada: {separar}')
